49a785f1c7452c96b795283fb24c6ca57faef449
windowwelder <welderules@duck.com>
2025-07-01 11:46:51 +0700

    Creating header and description

--- App.jsx
+++ App.jsx
@@ -3,7 +3,8 @@ import React from "react"
 export default function Hangman() {
     return (
         <main>
-            Game goes here
+            <header>Assembly: Endgame</header>
+            <p className="description">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </main>
     )
 }
