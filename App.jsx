36ccbe7589aa723d0d7eeb8f6ad3b65d3118ca84
windowwelder <welderules@duck.com>
2025-07-02 12:01:42 +0700

    feat: adding status section

--- App.jsx
+++ App.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 
-export default function Hangman() {
+export default function AssemblyEndgame() {
     return (
         <main>
-            <header>Assembly: Endgame</header>
-            <p className="description">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p className="description">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <div className="status-section">
+                <p className="win-text">You win!</p><p className="win-text" id="well-done">Well done!🎉</p>
+            </div>
         </main>
     )
 }
