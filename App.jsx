6a408271c6d7413f56af9d0f74d140322391b4d8
windowwelder <welderules@duck.com>
2025-07-08 23:24:11 +0700

    feat: confetti if the game is won

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { languages } from "./languages"
 import { clsx } from "clsx"
 import { getFarewellText } from "./utils"
+import Confetti from "react-confetti"
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = React.useState( () => getRandomWord())
@@ -139,6 +140,7 @@ export default function AssemblyEndgame() {
 
     return (
         <main>
+            {isGameWon && <Confetti />}
             <header>
                 <h1>Assembly: Endgame</h1>
                 <p className="description">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
