5071a32096b797bc544c1c0ac0b7ffddef733cb1
windowwelder <welderules@duck.com>
2025-07-12 12:23:14 +0700

    chore: remove code files from GitHub, keep README only

--- scrimba-learn_react-course/tenzies/src/Die.jsx
+++
@@ -1,6 +0,0 @@
-export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : null
-    }
-    return (<button onClick={ () => props.hold()} style={styles}>{props.value}</button>)
-}
