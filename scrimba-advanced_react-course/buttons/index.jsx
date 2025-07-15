0f70d8838606134ed7eb8b4dd1c9236938444c48
windowwelder <welderules@duck.com>
2025-07-15 14:33:24 +0700

    initial commit: buttons lesson

---
+++ scrimba-advanced_react-course/buttons/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <main>
+      {/* Your custom Button component here */}
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
