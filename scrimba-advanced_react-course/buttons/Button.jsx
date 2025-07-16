94c2c5f36d84a63b58a46f07164cef025f0e42b6
windowwelder <welderules@duck.com>
2025-07-16 14:05:28 +0700

    feat: using props.children

--- scrimba-advanced_react-course/buttons/Button.jsx
+++ scrimba-advanced_react-course/buttons/Button.jsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Button(props) {
     return (
-        <button>{props.text}</button>
+        <button>{props.children}</button>
     )
 }
--- scrimba-advanced_react-course/buttons/index.js
+++ scrimba-advanced_react-course/buttons/index.js
@@ -5,7 +5,7 @@ import Button from "./Button"
 function App() {
   return (
     <main>
-      <Button text=""/>
+      <Button>Buy now!</Button>
     </main>
   )
 }
