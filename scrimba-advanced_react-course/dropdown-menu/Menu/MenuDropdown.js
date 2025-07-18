39a62dd9964010bf9fa9bd8d0deb7be71bb0eefc
windowwelder <welderules@duck.com>
2025-07-18 13:58:24 +0700

    feat: passing props to a grandchild

--- scrimba-advanced_react-course/dropdown-menu/Menu/MenuDropdown.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/MenuDropdown.js
@@ -4,7 +4,12 @@ export default function MenuDropdown({ children, open }) {
     console.log(open, toggle)
     return open ? (
         <div className="menu-dropdown">
-            {children}
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    open,
+                    toggle
+                })
+            })}
         </div>
     ) : null
 }
