c2883f4d28a9bb0fd585cbe216af5711cf4a7c9f
windowwelder <welderules@duck.com>
2025-07-18 16:20:16 +0700

    feat: passing implicit value to component using condition

--- scrimba-advanced_react-course/dropdown-menu/Menu/MenuDropdown.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/MenuDropdown.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 export default function MenuDropdown({ children }) {
+    const open = React.useContext(MenuContext)
     return open ? (
         <div className="menu-dropdown">
             {children}
