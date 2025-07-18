279ce9095adac9d715962284610ef79be7ba3e69
windowwelder <welderules@duck.com>
2025-07-18 17:06:53 +0700

    feat: passing value using Context to Header

--- scrimba-advanced_react-course/themeContext/Header.js
+++ scrimba-advanced_react-course/themeContext/Header.js
@@ -4,11 +4,9 @@ import { ThemeContext } from "./App"
 export default function Header() {
     const value = React.useContext(ThemeContext)
     
-    const themeClass = value === "light" ? "light" : "dark";
-    const capitalized = themeClass[0].toUpperCase() + themeClass.slice(1);
     return (
-        <header className={`${themeClass}-theme`}>
-            <h1>{`${capitalized} Theme`}</h1>
+        <header className={`${value.theme}-theme`}>
+            <h1>{value.theme === "light" ? "Light" : "Dark"} Theme</h1>
         </header>
     )
 }
