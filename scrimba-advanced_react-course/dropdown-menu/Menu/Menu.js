08a020ad450f95fb16bbdc0857c667e44519e3ea
windowwelder <welderules@duck.com>
2025-07-18 12:59:35 +0700

    feat: using React.cloneElement() for first time

--- scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
@@ -1,22 +1,19 @@
 import React from "react"
 
-export default function Menu( props ) {
-    /**
-     * Note: leave the div className="menu" here and render
-     * the children inside that div. Notice this component will become
-     * significantly simpler by doing so 💡
-     * 
-     * Also, notice our state will be broken after we make 
-     * these changes - that's okay! We'll fix it soon. In the meantime,
-     * leave the useState() call and toggle() definitions alone. Your
-     * new version won't be using them, but we'll come back to them
-     * later.
-     */
-    const [open, setOpen] = React.useState(true);
+export default function Menu({ children }) {
+    const [open, setOpen] = React.useState(true)
 
+    function toggle() {
+        setOpen(prevOpen => !prevOpen)
+    }
+    
     return (
         <div className="menu">
-            {props.children}
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    isOpen: open, onClick: toggle
+                })
+            })}
         </div>
     )
 }
