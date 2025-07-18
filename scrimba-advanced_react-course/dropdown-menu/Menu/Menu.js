a58682a4dbad7e535f309217d73c035b3a8327c4
windowwelder <welderules@duck.com>
2025-07-18 16:12:00 +0700

    feat: apply Context to Menu exercise

--- scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
@@ -2,21 +2,20 @@ import React from "react"
 import MenuButton from "./MenuButton"
 import MenuDropdown from "./MenuDropdown"
 
+const MenuContext = React.createContext()
+
 export default function Menu({ children }) {
     const [open, setOpen] = React.useState(true)
 
     function toggle() {
         setOpen(prevOpen => !prevOpen)
     }
-
+    
     return (
-        <div className="menu">
-            {React.Children.map(children, (child) => {
-                return React.cloneElement(child, {
-                    open,
-                    toggle
-                })
-            })}
-        </div>
+        <MenuContext.Provider value={false}>
+            <div className="menu">
+                {children}
+            </div>
+        </MenuContext.Provider>
     )
 }
