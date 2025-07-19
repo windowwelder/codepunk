9738fb470f56586a2e35c69f655bb535063ae268
windowwelder <welderules@duck.com>
2025-07-19 07:38:03 +0700

    feat: adding Toggle.off

--- scrimba-advanced_react-course/toggle-context/Toggle/index.js
+++ scrimba-advanced_react-course/toggle-context/Toggle/index.js
@@ -1,6 +1,8 @@
 import Toggle from "./Toggle"
 import ToggleButton from "./ToggleButton"
 import ToggleOn from "./ToggleOn"
+import ToggleOff from "./ToggleOff"
 
 Toggle.Button = ToggleButton;
 Toggle.On = ToggleOn;
+Toggle.off = ToggleOff;
