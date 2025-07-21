6206ec2a8e96d01606b4f3ab71c063f7d6b60f1c
windowwelder <welderules@duck.com>
2025-07-21 14:59:58 +0700

    feat: returning menu component to index.js

--- scrimba-advanced_react-course/toggle-context/index.js
+++ scrimba-advanced_react-course/toggle-context/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Menu from "./dropdown-menu/Menu/index"
 import Star from "./Star"
             
 function App() {
   return (
     <>
       <Star onChange={ () => console.log("Hot damn!")}/>
+
+      <Menu onOpen={ () => console.log("Goddamnit!")}>
+        <Menu.Button>Menu</Menu.Button>
+        <Menu.Dropdown>
+          <Menu.Item>Home</Menu.Item>
+          <Menu.Item>About</Menu.Item>
+          <Menu.Item>Contact</Menu.Item>
+          <Menu.Item>Blog</Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
     </>
   )
 }
