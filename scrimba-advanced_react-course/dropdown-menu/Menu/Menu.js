d2873c46b9761a328670d8b0603a5979b2bc483b
windowwelder <welderules@duck.com>
2025-07-21 15:01:14 +0700

    feat: passing new function as a prop to Menu component

--- scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Toggle from "./Toggle/index";
 
-export default function Menu({ children }) {
+export default function Menu({ children, onOpen }) {
 
     return (
-            <Toggle>
+            <Toggle onToggle={onOpen}>
                 <div className="menu" role="menu" aria-expanded={open} aria-haspopup="true" >
                     {children}
                 </div>
