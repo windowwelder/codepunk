324458df61eefeb90c183232cafba30de2d072a7
windowwelder <welderules@duck.com>
2025-07-21 14:51:07 +0700

    feat: simplifying star component syntax in index.js

--- scrimba-advanced_react-course/toggle-context/index.js
+++ scrimba-advanced_react-course/toggle-context/index.js
@@ -2,25 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Toggle from "./Toggle/index";
 import { BsStar, BsStarFill } from "react-icons/bs"
+import Star from "./Star"
             
 function App() {
   return (
     <>
-      <Toggle>
-        <Toggle.Button>
-          <Toggle.On>
-              <BsStarFill className="star filled" />
-          </Toggle.On>
-          <Toggle.Off>
-              <BsStar className="star " />
-          </Toggle.Off>
-        </Toggle.Button>
-      </Toggle>
-
-            <Menu>
-                <Menu.Button>"Menu"</Menu.Button>
-                <Menu.Dropdown><Menu.Item>"options"</Menu.Item></Menu.Dropdown>
-            </Menu>
+      <Star />
     </>
   )
 }
