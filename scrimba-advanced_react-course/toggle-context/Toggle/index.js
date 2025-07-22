81ca738a3f837b0c4204ce5687f4d66b82beb45d
windowwelder <welderules@duck.com>
2025-07-22 13:10:45 +0700

    feat: attaching ToggleDisplay component to Toggle and fixing bug

--- scrimba-advanced_react-course/toggle-context/Toggle/index.js
+++ scrimba-advanced_react-course/toggle-context/Toggle/index.js
@@ -2,7 +2,11 @@ import Toggle from "./Toggle"
 import ToggleButton from "./ToggleButton"
 import ToggleOn from "./ToggleOn"
 import ToggleOff from "./ToggleOff"
+import ToggleDisplay from "./ToggleDisplay"
 
 Toggle.Button = ToggleButton;
 Toggle.On = ToggleOn;
 Toggle.off = ToggleOff;
+Toggle.Display = ToggleDisplay
+
+export default Toggle
