385749f874820f7dd964e9f31243c54fbe0070ae
windowwelder <welderules@duck.com>
2025-07-22 17:49:10 +0700

    feat: providing context to Menu component

--- scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
@@ -1,11 +1,15 @@
 import React from "react"
 import useToggle from "../../toggle-context/hooks/useToggle"
 
+const MenuContext = React.createContext()
+
 export default function Menu({ children }) {
     const [open, toggleOpen] = useToggle(false)
     return (
+            <MenuContext.Provider value={{open, toggleOpen}}>
                 <div className="menu" role="menu" aria-expanded={open} aria-haspopup="true" >
                     {children}
                 </div>
+            </MenuContext.Provider>
     )
 }
