89936e9e67ffd7e9ac2644830be077468c1cc737
windowwelder <welderules@duck.com>
2025-07-22 15:33:47 +0700

    feat: adding useToggle logic to Menu component

--- scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
+++ scrimba-advanced_react-course/dropdown-menu/Menu/Menu.js
@@ -1,7 +1,8 @@
 import React from "react"
+import useToggle from "../../toggle-context/hooks/useToggle"
 
-export default function Menu({ children, onOpen }) {
-
+export default function Menu({ children }) {
+    const [open, toggleOpen] = useToggle(false)
     return (
                 <div className="menu" role="menu" aria-expanded={open} aria-haspopup="true" >
                     {children}
