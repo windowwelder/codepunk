210596f43d394ce8e8b785ac908281f2183e7161
windowwelder <welderules@duck.com>
2025-07-22 23:01:26 +0700

    feat: adding initial value to state in our custom hook

--- scrimba-advanced_react-course/toggle-context/hooks/useToggle.js
+++ scrimba-advanced_react-course/toggle-context/hooks/useToggle.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-export default function useToggle() {
-    const [on, setOn] = React.useState(false)
+export default function useToggle( initialValue = false ) {
+    const [on, setOn] = React.useState(initialValue)
 
     function toggle() {
         setOn(prevOn => !prevOn)
