42d6752dc36015dcaf1c02d00f8068d023aa3e19
windowwelder <welderules@duck.com>
2025-07-23 12:10:18 +0700

    feat: reconfiguring Vite again

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, '.\scrimba-advanced_react-course\solo-projects\component library\badges'),
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
