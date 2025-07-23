fa1832d290f42cd213a95d2fab5a9ddcde33c6a5
windowwelder <welderules@duck.com>
2025-07-23 11:39:38 +0700

    feat: configuring jsx file

---
+++ scrimba-advanced_react-course/solo-projects/component library/badges/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return (
+        <h1>first component</h1>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);   
