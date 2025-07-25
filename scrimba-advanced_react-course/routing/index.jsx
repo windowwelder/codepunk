659757f9843dec034f3d36d7e32730e21a28e0a2
windowwelder <welderules@duck.com>
2025-07-25 14:22:20 +0700

    chore: reorganization

--- scrimba-advanced_react-course/routing/index.jsx
+++ scrimba-advanced_react-course/routing/index.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import About from "./About"
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-</BrowserRouter>
-);
+function Home() {
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>About page goes here! 🎉</h1>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
