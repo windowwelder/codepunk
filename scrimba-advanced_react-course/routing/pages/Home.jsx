310a1e15a93c28a4dc765b28d7b784fb4979a2da
windowwelder <welderules@duck.com>
2025-07-25 19:26:47 +0700

    feat: adding text containers to Home and About pages

--- scrimba-advanced_react-course/routing/pages/Home.jsx
+++ scrimba-advanced_react-course/routing/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function Home() {
   return (
-    <h1>Hello, React Router!</h1>
+    <div className="home-container">
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
+    </div>
   )
 }
