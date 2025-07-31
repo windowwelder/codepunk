aea1f86f3ee85011e7c0eff2438f4ed4b05c188f
windowwelder <welderules@duck.com>
2025-07-31 23:39:05 +0700

    feat: creating VanDetail component and using params first time

---
+++ scrimba-advanced_react-course/routing/pages/VanDetail.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+
+export default function VanDetail() {
+    const params = useParams()
+    console.log(params)
+    return <h1>Van detail page goes here</h1>
+}
