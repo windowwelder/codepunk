14c3849a970c24f18edd0ecabadd6a17d4bb643f
windowwelder <welderules@duck.com>
2025-07-31 12:43:29 +0700

    feat: adding link

--- scrimba-advanced_react-course/routing/pages/Vans.jsx
+++ scrimba-advanced_react-course/routing/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Vans() {
     const [ vans, setVans ] = React.useState(null);
@@ -10,14 +11,16 @@ export default function Vans() {
     )
 
     const vanElements = vans.map(van => (
-        <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
+        <Link to={`/vans/${van.id}`}>
+            <div key={van.id} className="van-tile">
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
             </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </div>
+        </Link>
     ))
 
     return (
