9f471a07ce3661e637020988627e5ebbfcf79080
windowwelder <welderules@duck.com>
2025-07-31 11:59:50 +0700

    feat: fetching data from fake api and rendering it

--- scrimba-advanced_react-course/routing/pages/Vans.jsx
+++ scrimba-advanced_react-course/routing/pages/Vans.jsx
@@ -1,7 +1,28 @@
 import React from "react"
 
 export default function Vans() {
+    const [ vans, setVans ] = React.useState(null);
+    
+    React.useEffect(
+        () => {
+            fetch("/api/vans").then( res => res.json() ).then( data => setVans(data.vans) )
+            }, []
+    )
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ))
+
     return (
-        <h1>Vans page goes here 🚐</h1>
+        <>
+        {vanElements}
+        </>
     )
 }
