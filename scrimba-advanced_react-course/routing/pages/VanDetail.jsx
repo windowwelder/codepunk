ffe2cd8366f55fe47b38aa14b500acb28d8c2c9d
windowwelder <welderules@duck.com>
2025-08-04 13:08:24 +0700

    feat: fetch data to VanDetail every time params.id changes

--- scrimba-advanced_react-course/routing/pages/VanDetail.jsx
+++ scrimba-advanced_react-course/routing/pages/VanDetail.jsx
@@ -3,6 +3,12 @@ import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
     const params = useParams()
-    console.log(params)
+   
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [params.id])
+
     return <h1>Van detail page goes here</h1>
 }
