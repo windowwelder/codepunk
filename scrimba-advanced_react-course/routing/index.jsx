bbee584d96f90e20dce7775f14e352ebe85ad397
windowwelder <welderules@duck.com>
2025-08-05 19:49:04 +0700

    fix: fixing defunct nested routes

--- scrimba-advanced_react-course/routing/index.jsx
+++ scrimba-advanced_react-course/routing/index.jsx
@@ -15,7 +15,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Host />}>
+          <Route path="/host" element={<Dashboard />}>
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
--- scrimba-advanced_react-course/routing/pages/Host/Dashboard.jsx
+++ scrimba-advanced_react-course/routing/pages/Host/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
 
 export default function Dashboard() {
     return(
-        <h1>Here is Dashboard</h1>
+        <>
+            <h1>Here is Dashboard</h1>
+            <Outlet />
+        </>
     )
 }
