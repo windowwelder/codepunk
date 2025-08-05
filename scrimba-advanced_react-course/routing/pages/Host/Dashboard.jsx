f6c61ba8ac648abef9bdc55c21847e1735d35deb
windowwelder <welderules@duck.com>
2025-08-05 19:34:46 +0700

    feat: creating missing components

---
+++ scrimba-advanced_react-course/routing/pages/Host/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Dashboard() {
+    return(
+        <h1>Here is Dashboard</h1>
+    )
+}
---
+++ scrimba-advanced_react-course/routing/pages/Host/Reviews.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function Reviews() {
+    return (
+        <h1>Here are reviews</h1>
+    )
+}
