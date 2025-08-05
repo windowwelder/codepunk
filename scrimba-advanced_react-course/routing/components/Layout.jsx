cfd188ac07316b00279df6224d29a4c8648f874d
windowwelder <welderules@duck.com>
2025-08-05 19:03:42 +0700

    feat: creating Layout component with the Outlet

---
+++ scrimba-advanced_react-course/routing/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+
+export default function Layout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <h1>This is the Layout Route</h1>
+        </>
+    )
+}
