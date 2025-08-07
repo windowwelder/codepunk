d3da4f24edaac2c09b1d58994817e56833e48784
windowwelder <welderules@duck.com>
2025-08-07 16:50:06 +0700

    fix: adding missing imports

--- scrimba-advanced_react-course/routing/index.jsx
+++ scrimba-advanced_react-course/routing/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVansDetail from "./pages/Host/HostVansDetail"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+
+import "./server"
 
 function App() {
   return (
