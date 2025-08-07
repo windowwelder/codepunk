69c200e741dc944af3940566290bf57fae1ff171
windowwelder <welderules@duck.com>
2025-08-07 15:25:40 +0700

    feat: changing links in main navbar to navlinks

--- scrimba-advanced_react-course/routing/components/Header.jsx
+++ scrimba-advanced_react-course/routing/components/Header.jsx
@@ -6,9 +6,9 @@ export function Header() {
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
-          <Link to="/host">Host</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
+          <NavLink to="/host" className={ ({isActive}) => isActive ? "link-active" : null}>Host</NavLink>
+          <NavLink to="/about" className={ ({isActive}) => isActive ? "link-active" : null}>About</NavLink>
+          <NavLink to="/vans" className={ ({isActive}) => isActive ? "link-active" : null}>Vans</NavLink>
         </nav>
       </header>
     )
