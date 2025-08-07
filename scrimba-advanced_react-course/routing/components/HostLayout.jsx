1d89483731c4d7de8a4cddb899706ca7455aef0a
windowwelder <welderules@duck.com>
2025-08-07 15:47:42 +0700

    feat: changing links to navlinks in secondary navbar with a bit of the styling

--- scrimba-advanced_react-course/routing/components/HostLayout.jsx
+++ scrimba-advanced_react-course/routing/components/HostLayout.jsx
@@ -2,11 +2,30 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default function HostLayout() {
+    const activeStyle = {
+        fontWeight: "bold", 
+        textDecoration: "underline", 
+        color: "#161616"}
     return (
         <>
-            <Link to="/host">Dashboard</Link>
-            <Link to="/host/income">Income</Link>
-            <Link to="/host/reviews">Reviews</Link>
+            <NavLink 
+                    to="/host" 
+                    style={ ({isActive}) => isActive ? activeStyle : null}
+                >
+                    Dashboard
+                </NavLink>
+                <NavLink 
+                    to="/host/income" 
+                    style={ ({isActive}) => isActive ? activeStyle : null}
+                >
+                    Income
+                </NavLink>
+                <NavLink 
+                    to="/host/reviews" 
+                    style={ ({isActive}) => isActive ? activeStyle : null}
+                >
+                    Reviews
+                </NavLink>
         </>
     )
 }
