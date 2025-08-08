a68902e06c11096699da69276ecf682e53b8dddb
windowwelder <welderules@duck.com>
2025-08-08 14:31:40 +0700

    feat: adding missing HostVanDetail component

--- scrimba-advanced_react-course/routing/pages/Host/HostVanDetail.jsx
+++ scrimba-advanced_react-course/routing/pages/Host/HostVanDetail.jsx
@@ -1,23 +1,42 @@
 import React from "react"
-import {useParams} from "react-router-dom"
+import { useParams, Link, Outlet } from "react-router-dom"
 
 export default function HostVanDetail() {
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
 
-    const params = useParams();
-    const [van,setVan] = React.useState([])
-    
-    React.useEffect(
-        fetch(`/api/host/vans/${params.id}`).then( res => res.json() ).then(
-            data => setVan(data.van)
-        )
-    ,[])
-    
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans))
+    }, [])
+
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
     return (
-        <div className="van-host-container">
-            <h1>{van.name}</h1>
-            <img src={van.imageUrl} />
-            <p>{van.price}</p>
-            <p>{van.type}</p>
-        </div>        
-        )
+        <section>
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+                <Outlet />
+            </div>
+        </section>
+    )
 }
