cc3dace0b247fc68ca1dab8e7f9dfb912c2ae7a5
windowwelder <welderules@duck.com>
2025-08-08 13:28:05 +0700

    feat: creating HostVanDetail component

--- scrimba-advanced_react-course/routing/pages/Host/HostVansDetail.jsx
+++ scrimba-advanced_react-course/routing/pages/Host/HostVansDetail.jsx
@@ -1,7 +1,23 @@
 import React from "react"
+import {useParams} from "react-router-dom"
 
-export default function HostVans() {
+export default function HostVanDetail() {
+
+    const params = useParams();
+    const [van,setVan] = React.useState([])
+    
+    React.useEffect(
+        fetch(`/api/host/vans/${params.id}`).then( res => res.json() ).then(
+            data => setVan(data.van)
+        )
+    ,[])
+    
     return (
-        <h1>Here goes nothing</h1>
-    )
+        <div className="van-host-container">
+            <h1>{van.name}</h1>
+            <img src={van.imageUrl} />
+            <p>{van.price}</p>
+            <p>{van.type}</p>
+        </div>        
+        )
 }
