00ab03c401dae2591a619ded3c807ac51fa59b2c
windowwelder <welderules@duck.com>
2025-08-27 20:53:50 +0700

    clean-up: old fetch function

--- scrimba-advanced_react-course/routing/api.js
+++ scrimba-advanced_react-course/routing/api.js
@@ -37,24 +37,11 @@ export async function getVan(id) {
     const snapshot = await getDoc(docRef)
     const van = {
         ...snapshot.data(),
-        id: doc.id
+        id: snapshot.id
         }
     return van
 }
 
-/* export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-} */
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
