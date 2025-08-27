386c94db88c6bebdfc65d9ab51276170d27cd737
windowwelder <welderules@duck.com>
2025-08-27 23:25:19 +0700

    initial commit: react performance project setup

---
+++ scrimba-advanced_react-course/performance/index.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import ProductsList from "./ProductsList"
+
+function App() {
+  const [count, setCount] = React.useState(0)
+  const [showProducts, setShowProducts] = React.useState(false)
+
+  function increment() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function decrement() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  return (
+    <>
+      <h1>The current count is {count}</h1>
+      <button className="button" onClick={decrement}>
+        -
+            </button>
+      <button className="button" onClick={increment}>
+        +
+            </button>
+      <br />
+      <br />
+      <button
+        className="button"
+        onClick={() => setShowProducts(prev => !prev)}
+      >
+        Show Products
+            </button>
+      <br />
+      <br />
+      <div className="products-list">
+        {showProducts && <ProductsList />}
+      </div>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
--- scrimba-advanced_react-course/routing/pages/Host/HostVanDetail.jsx
+++ scrimba-advanced_react-course/routing/pages/Host/HostVanDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useParams, Link, Outlet } from "react-router-dom"
-import { getHostVans } from "../../api"
+import { getVan } from "../../api"
 
 export default function HostVanDetail() {
     const [currentVan, setCurrentVan] = React.useState(null)
@@ -13,7 +13,7 @@ export default function HostVanDetail() {
         async function loadVans() {
             setLoading(true)
             try {
-                const data = await getHostVans(id)
+                const data = await getVan(id)
                 setCurrentVan(data)
             } catch (err) {
                 setError(err)
