3212accb061f326106747adafcdbda68bceab660
windowwelder <welderules@duck.com>
2025-09-05 14:11:20 +0700

    feat: typing almost everything we can

---
+++ scrimba-learn_typescript/index.ts
@@ -0,0 +1,43 @@
+type Menu = {
+    name: string,
+    price: number
+}
+
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+type Order = {
+    id: number,
+    pizza: Menu,
+    status: string
+}
+
+let cashInRegister = 100
+let nextOrderId: number = 1
+const orderQueue: Order[] = []
+
+function addNewPizza(pizzaObj: Menu) {
+    menu.push(pizzaObj)
+}
+
+function placeOrder(pizzaName: string) {
+    const selectedPizza = menu.find( pizza => pizza.name === pizzaName )
+    if (!selectedPizza) {
+        console.error(`${pizzaName} does not exist in the menu`)
+        return
+    }
+    cashInRegister += selectedPizza.price;
+    const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId: number) {
+    const order = orderQueue.find( order => order.id === orderId );
+    order.status = "completed";
+    return order
+}
