22c4dba3e9edc622a4c47b5fd055400a0e872172
windowwelder <welderules@duck.com>
2025-09-11 23:13:37 +0700

    initial setup: fixed-size sliding window

---
+++ Algorithms/sliding_window/fixed-size_window.js
@@ -0,0 +1,10 @@
+//You are given an array of integers nums and an integer k.
+//Find the maximum sum of any subarray of length k.
+
+const nums = [2, 1, 5, 1, 3, 2]
+const k = 3
+
+
+
+console.log(k)
+
