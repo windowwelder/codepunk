726dafc09822028395265bdb9dcdad4a4083635f
windowwelder <welderules@duck.com>
2025-09-11 23:39:09 +0700

    implementing fixed-size window algorithm

--- Algorithms/sliding_window/fixed-size_window.js
+++ Algorithms/sliding_window/fixed-size_window.js
@@ -3,8 +3,13 @@
 
 const nums = [2, 1, 5, 1, 3, 2]
 const k = 3
+let sum = nums.slice(0, k).reduce((acc,val) => acc + val, 0)
+console.log(nums.slice(0, k - 1))
+
+for ( let i = 0; i < k + 1; i++) {
+    i === 0 ? null : sum = sum - nums[i - 1] + nums[i + 2]
+    console.log(sum)
+}
 
 
-
-console.log(k)
 
