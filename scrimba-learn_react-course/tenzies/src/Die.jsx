3db36d3027d06539a344c7f496ab545878337dd8
windowwelder <welderules@duck.com>
2025-09-24 17:44:36 +0700

    chore: excess code removal

--- scrimba-learn_react-course/tenzies/src/Die.jsx
+++ scrimba-learn_react-course/tenzies/src/Die.jsx
@@ -1,16 +1,3 @@
 export default function Die(props) {
-    const theme = {
-        size: "60px"
-    }
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "#e4e9e8ff",
-        width: theme.size,
-        height: theme.size,
-        margin: "5px",
-        border: "0.5px solid #070908ff",
-        borderRadius: "5px",
-        fontSize: "30px",
-        fontFamily: "Inter, sans-serif"
-    }
-    return (<button onClick={ () => props.hold()} /* style={styles} */>{props.value}</button>)
+    return (<button onClick={ () => props.hold()}>{props.value}</button>)
 }
