1514c30d382ae6a8d259010e2d30b6054240de72
windowwelder <welderules@duck.com>
2025-09-24 14:01:52 +0700

    style: style variables and dice styling

--- scrimba-learn_react-course/tenzies/src/Die.jsx
+++ scrimba-learn_react-course/tenzies/src/Die.jsx
@@ -1,6 +1,11 @@
 export default function Die(props) {
+    const theme = {
+        size: "30px"
+    }
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : null,
+        width: theme.size,
+        height: theme.size
     }
     return (<button onClick={ () => props.hold()} style={styles}>{props.value}</button>)
 }
